8dab6e9e4c2b9ba3ea18e55c924ada379d6745fb
home-notebook <kim.songhyon@gmail.com>
2025-03-08 21:49:11 +0900

    ALLOWED_HOSTS 개선

--- src/settings.py
+++ src/settings.py
@@ -19,8 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', env('WEB_DOMAIN')]
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # Application definition
 
